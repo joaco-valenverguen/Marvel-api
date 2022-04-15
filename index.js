38377b8b9fd888e643198dc234111dfe87701975
joaco-valenverguen <69991957+joaco-valenverguen@users.noreply.github.com>
2022-04-15 12:59:38 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const key = "88593eb394c8d654d590f6e5e7fa356e";
+const hash = "ba2a8c7c15bda7cb7d70670ceca21ebf";
+const content = document.querySelector(".content");
+const marvel = {
+  render: () => {
+    const urlAPI = `https://gateway.marvel.com/v1/public/characters?ts=1&apikey=${key}&hash=${hash}`;
+    const content = document.querySelector("#marvel-row");
+    let contentHTML = "";
+
+    fetch(urlAPI)
+      .then((res) => res.json())
+      .then((json) => {
+        for (const hero of json.data.results) {
+          let urlHero = hero.urls[0].url;
+          contentHTML += `
+              <div class="col-md-4">
+                  <a href="${urlHero}" target="_blank">
+                    <img src="${hero.thumbnail.path}.${hero.thumbnail.extension}" alt="${hero.name}" class="img-thumbnail">
+                  </a>
+                  <h3 class="title">${hero.name}</h3>
+              </div>`;
+        }
+        content.innerHTML = contentHTML;
+      });
+  },
+};
+marvel.render();
